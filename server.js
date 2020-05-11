0c1cfee2ed5481c02b82f804cef9721c9104d49e
rosanaporto <rosanaipg25@gmail.com>
2020-05-11 22:54:49 +0000

    versão 2 com CRUD

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const server = express();
 server.use(express.json());
 
 const tarefas = [
-    {id: 1, disciplina: 'LP3', data: '12', entregue: 'Sim'},
-    {id: 2, disciplina: 'PO', data: '15', entregue: 'Não'}
+    {id: 1, disciplina: 'LP3', date: ('12-05-2020'), entregue: 'Sim'},
+    {id: 2, disciplina: 'PO', date: ('15-05-2020'), entregue: 'Não'}
 ]
 
 server.get('/tarefa', function(request, response) {
@@ -15,20 +15,20 @@ server.get('/tarefa', function(request, response) {
 
 server.post('/tarefa', function(request, response) {
 
-    const {id, disciplina, data, entregue} = request.body;
+    const {id, disciplina, date, entregue} = request.body;
 
-    tarefas.push({id, disciplina, data, entregue})
+    tarefas.push({id, disciplina, date, entregue})
     response.status(204).send();
 })
 
 server.put('/tarefa/:id', function(request, response){
     const id = request.params.id;
-    const {disciplina, data, entregue} = request.body;
+    const {disciplina, date, entregue} = request.body;
 
     for(let i = 0; i < tarefas.length; i++){
         if(tarefas[i].id == id) {
             tarefas[i].disciplina = disciplina;
-            tarefas[i].data= data;
+            tarefas[i].date= date;
             tarefas[i].entregue = entregue;
             break;
         }
@@ -54,4 +54,4 @@ server.delete('/tarefa/:id', function(request, response) {
 
 
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000); 
